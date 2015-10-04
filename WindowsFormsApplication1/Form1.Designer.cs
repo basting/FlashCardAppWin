4ff799a16048e2654b5b88fb5d2c82739046ea4e
basting <bastin.gomez@gmail.com>
2015-10-04 10:43:14 -0400

    First commit of the project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.txtFlashCard = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRight);
+            this.panel1.Controls.Add(this.btnLeft);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(315, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRight.BackgroundImage")));
+            this.btnRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRight.Location = new System.Drawing.Point(161, 3);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(152, 73);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLeft.BackgroundImage")));
+            this.btnLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLeft.Location = new System.Drawing.Point(3, 3);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(152, 73);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // txtFlashCard
+            // 
+            this.txtFlashCard.Location = new System.Drawing.Point(5, 85);
+            this.txtFlashCard.Multiline = true;
+            this.txtFlashCard.Name = "txtFlashCard";
+            this.txtFlashCard.ReadOnly = true;
+            this.txtFlashCard.Size = new System.Drawing.Size(310, 206);
+            this.txtFlashCard.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 291);
+            this.Controls.Add(this.txtFlashCard);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.TextBox txtFlashCard;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList flashCardList = new ArrayList();
+        private int currentPosition = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            initFlashCards();
+        }
+
+        private void initFlashCards()
+        {
+            flashCardList.Add("StringBuffer is thread-safe \n "+
+                               "StringBuilder is not thread-safe");
+
+            flashCardList.Add("HashTable is thread-safe \n " +
+                               "HashMap is not thread-safe");
+
+            flashCardList.Add("Two ways to create threads - Runnable & Thread");
+
+            btnLeft.Click += new EventHandler(this.btnLeft_Click);
+            btnRight.Click += new EventHandler(this.btnRight_Click);
+            txtFlashCard.Text = flashCardList[currentPosition].ToString();
+            
+            /*
+            if(currentPosition == 0)
+            {
+                btnLeft.Enabled = false;
+            }
+            else
+            {
+                btnLeft.Enabled = true;
+            }
+
+            if (currentPosition == flashCardList.Count-1)
+            {
+                btnRight.Enabled = false;
+            }
+            else
+            {
+                btnRight.Enabled = true;
+            }*/
+
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (currentPosition >= flashCardList.Count - 1)
+            {
+                return;
+            }
+            txtFlashCard.Text = flashCardList[++currentPosition].ToString();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (currentPosition <= 0)
+            {
+                return;
+            }
+            txtFlashCard.Text = flashCardList[--currentPosition].ToString();
+        }
+        
+    }
+}
